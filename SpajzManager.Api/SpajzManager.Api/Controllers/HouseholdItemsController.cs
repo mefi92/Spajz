9d7743c6944bb2cc44112b41967baad472c8c291
mefi92 <sgrcsont@gmail.com>
2025-01-21 21:45:46 +0100

    Xml formatter added

--- SpajzManager.Api/SpajzManager.Api/Controllers/HouseholdItemsController.cs
+++ SpajzManager.Api/SpajzManager.Api/Controllers/HouseholdItemsController.cs
@@ -1,11 +1,45 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SpajzManager.Api.Models;
 
 namespace SpajzManager.Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/households/{householdId}/householditems")]
     [ApiController]
     public class HouseholdItemsController : ControllerBase
     {
+        [HttpGet]
+        public ActionResult<IEnumerable<HouseholdItemDto>> GetHouseholdItems(int householdId)
+        {
+            var household = HouseholdDataStore.Current.Households
+                .FirstOrDefault(h => h.Id == householdId);
+
+            if (household == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(household.Items);
+        }
+
+        [HttpGet("householditemid")]
+        public ActionResult<HouseholdItemDto> GetHouseholdItem(
+            int householdId, int householdItemId) 
+        {
+            var household = HouseholdDataStore.Current.Households
+                .FirstOrDefault(h => h.Id == householdId);
+            if (household == null)
+            {
+                return NotFound();
+            }
+
+            var item = household.Items.FirstOrDefault(i => i.Id == householdItemId);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(item);
+        }
     }
 }
--- SpajzManager.Api/SpajzManager.Api/Program.cs
+++ SpajzManager.Api/SpajzManager.Api/Program.cs
@@ -1,10 +1,18 @@
+using Microsoft.AspNetCore.StaticFiles;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.ReturnHttpNotAcceptable = true;
+}).AddXmlDataContractSerializerFormatters();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
